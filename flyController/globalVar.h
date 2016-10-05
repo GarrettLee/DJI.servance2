fc049d909180abda93e958b021754caf4993d53f
garrett <672757671@qq.com>
2016-10-05 14:33:13 +0800

    调试更改

--- flyController/globalVar.h
+++ flyController/globalVar.h
@@ -3,7 +3,7 @@
 #define sendBySocket 0					//为1时会启动socket1，把解码后的bitmap图片发送到本机的其他应用，
 #define ENABLE_WHOLENESS_CHECK 0;		//为1时会启动码流完整性检查，否则默认认为码流是完整的
 #define DECODE_NATIVE_CODE 0			//为1时从项目目录下读取作为例子用的本地h.264文件做解码，这样就能方便解码器的调试
-#define FLIGHT_CONTROL 0				//为1时会启用大疆的飞行控制
+#define FLIGHT_CONTROL 1				//为1时会启用大疆的飞行控制
 //#define SERVANCE_IP "192.168.0.114" 
 #define SERVANCE_IP "172.20.10.9"		//服务器（本机）ip地址
 #define MATLAB_IP "127.0.0.1"
